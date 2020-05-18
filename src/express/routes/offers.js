c9dbbe91ba3f7cdca5ebeb6b3648ea0c6ec35f0e
Dmitriy <d.orlov777@gmail.com>
2020-05-18 20:45:11 +0300

    add pug templates

--- src/express/routes/offers.js
+++ src/express/routes/offers.js
@@ -5,19 +5,19 @@ const {Router} = require(`express`);
 const offersRouter = new Router();
 
 offersRouter.get(`/add`, (req, res) => {
-  res.send(`/offers/add`);
+  res.render(`new-ticket`);
 });
 
 offersRouter.get(`/:id`, (req, res) => {
-  res.send(`/offers/${req.params.id}`);
+  res.render(`ticket`);
 });
 
 offersRouter.get(`/category/:id`, (req, res) => {
-  res.send(`/offers/category/${req.params.id}`);
+  res.render(`category`);
 });
 
 offersRouter.get(`/edit/:id`, (req, res) => {
-  res.send(`/offers/edit/${req.params.id}`);
+  res.render(`ticket-edit`);
 });
 
 module.exports = {
--- src/express/routes/register.js
+++ src/express/routes/register.js
@@ -5,7 +5,7 @@ const {Router} = require(`express`);
 const registerRouter = new Router();
 
 registerRouter.get(`/`, (req, res) => {
-  res.send(`/register`);
+  res.render(`sign-up`);
 });
 
 module.exports = {
