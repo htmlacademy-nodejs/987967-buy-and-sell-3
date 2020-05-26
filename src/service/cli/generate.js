890cd46882a8851b9a3f7558f6174bc3a0c7ad0b
Dmitriy <d.orlov777@gmail.com>
2020-05-26 14:16:47 +0300

    add 'id' and 'comments' in the generate.js

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -3,7 +3,8 @@
 const {getRandomBoolean, getRandomUniqueElements, getRandomInt, getRandomElement, readContent} = require(`../../utils`);
 const fs = require(`fs`);
 const chalk = require(`chalk`);
-const {ExitCode, MOCK_FILE_NAME} = require(`../const`);
+const {nanoid} = require(`nanoid`);
+const {ExitCode, MOCK_FILE_NAME, MAX_ID_LENGTH} = require(`../const`);
 
 const DEFAULT_COUNT = 1;
 const MAX_COUNT = 1000;
@@ -28,6 +29,11 @@ const DescriptionLength = {
   MAX: 5,
 };
 
+const CommentLength = {
+  MIN: 0,
+  MAX: 3,
+};
+
 const PictureNumber = {
   MIN: 1,
   MAX: 16,
@@ -37,15 +43,26 @@ const DataFileName = {
   TITLE: `titles.txt`,
   DESCRIPTION: `sentences.txt`,
   CATEGORY: `categories.txt`,
+  COMMENT: `comments.txt`,
 };
 
-const generateOffer = ({titles, descriptions, categories}) => ({
+const generateComments = (data) => {
+  const comments = getRandomUniqueElements(data, getRandomInt(CommentLength.MIN, CommentLength.MAX));
+  return comments.map((it) => ({
+    id: nanoid(MAX_ID_LENGTH),
+    text: it
+  }));
+};
+
+const generateOffer = ({titles, descriptions, categories, comments}) => ({
+  id: nanoid(MAX_ID_LENGTH),
   title: getRandomElement(titles),
   picture: `item${getRandomInt(PictureNumber.MIN, PictureNumber.MAX)}.jpg`.replace(/m(\d)\./, `m0$1.`),
   description: getRandomUniqueElements(descriptions, getRandomInt(DescriptionLength.MIN, DescriptionLength.MAX)).join(`\n`),
   type: getRandomBoolean() ? OfferType.OFFER : OfferType.SALE,
   sum: getRandomInt(SumRestrict.MIN, SumRestrict.MAX),
   category: getRandomElement(categories),
+  comments: generateComments(comments),
 });
 
 const generate = (count, data) => Array(count).fill(``).map(() => generateOffer(data));
@@ -56,6 +73,7 @@ const createMockFile = async (count) => {
       titles: await readContent(`./data/${DataFileName.TITLE}`),
       descriptions: await readContent(`./data/${DataFileName.DESCRIPTION}`),
       categories: await readContent(`./data/${DataFileName.CATEGORY}`),
+      comments: await readContent(`./data/${DataFileName.COMMENT}`),
     };
 
     await fs.promises.writeFile(MOCK_FILE_NAME, JSON.stringify(generate(count, data)));
--- src/service/const.js
+++ src/service/const.js
@@ -19,6 +19,8 @@ module.exports = {
     NOT_FOUND: `Not found`,
     OK: `Ok`,
     SERVER_ERROR: `Server error`,
-  }
+  },
+
+  MAX_ID_LENGTH: 6,
 };
 
